5750a88098a4eff64d20981531dfd6efe93fc503
Harshitharsh12 <kairaharshit888@gmail.com>
2024-08-15 11:29:56 +0530

    Update CountriesList.jsx

--- project2/src/assets/Components/CountriesList.jsx
+++ project2/src/assets/Components/CountriesList.jsx
@@ -1,27 +1,8 @@
-// import CountriesData from "../CountriesData";
 import { useEffect, useState } from "react";
 import CountryCard from "./CountryCard";
 import CountriesListShimmer from "./CountriesListShimmer";
 const CountriesList = ({ query }) => {
   const [countriesData, setCountriesData] = useState([]);
-  // const [count,setCount]=useState(0)
-  // useEffect(()=>{
-  // fetch('https://restcountries.com/v3.1/all')
-  // .then((res)=>res.json())
-  // .then((data)=>{
-  //   setCountriesData(data)
-  // })
-  // },[])
-
-  // useEffect(()=>{
-  // console.log("hi")
-  // },[count])
-
-  // or
-
-  // useEffect(()=>{
-  //   console.log("hi")
-  // },[count,countriesData])
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
@@ -38,13 +19,8 @@ const CountriesList = ({ query }) => {
     };
   }, []);
 
-  // console.log(CountriesData)
   return (
-    // <.>
-    // <h1>{count}</h1>
-    // <button onClick={()=>{
-    // setCount(count+1)
-    // }}>Increase Count</button>
+  
 
     !countriesData.length ? (
       <CountriesListShimmer />
