6d7ffefe428fa3e59ed502f1085e99ca42d95940
Harshitharsh12 <kairaharshit888@gmail.com>
2024-08-15 11:33:46 +0530

    Update CountriesListShimmer.jsx

--- project2/src/assets/Components/CountriesListShimmer.jsx
+++ project2/src/assets/Components/CountriesListShimmer.jsx
@@ -2,23 +2,7 @@ import './CountriesListShimmer.css'
 const CountriesListShimmer = () => {
   return (
     <div className="countries-container">
-      {/* <div className=" shimmer-card country-card"></div>
-      <div className=" shimmer-card country-card"></div>
-      <div className=" shimmer-card country-card"></div>
-      <div className=" shimmer-card country-card"></div>
-      <div className=" shimmer-card country-card"></div>
-      <div className=" shimmer-card country-card"></div>
-      <div className=" shimmer-card country-card"></div>
-      <div className=" shimmer-card country-card"></div>
-      <div className=" shimmer-card country-card"></div>
-      <div className=" shimmer-card country-card"></div>
-      <div className=" shimmer-card country-card"></div>
-      <div className=" shimmer-card country-card"></div>
-      <div className=" shimmer-card country-card"></div>
-      <div className=" shimmer-card country-card"></div>
-      <div className=" shimmer-card country-card"></div> */}
-       
-       {/* or */}
+     
 {Array.from({length:15}).map((el,i)=>{
     return <div className=" shimmer-card country-card" key={i}></div>
   })    }
