13a1dacb1f692c13c6eacb75875c83bad5569543
Harshitharsh12 <kairaharshit888@gmail.com>
2024-08-15 11:51:40 +0530

    Update App.jsx

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,9 +1,6 @@
-// import { useState } from "react";
 import Header from "./assets/Components/Header";
-// import "./App.css";
 import { Outlet } from "react-router-dom";
 const App = () => {
-  // const [query,setQuery]=useState("")
   return (
     <>
       <Header />
